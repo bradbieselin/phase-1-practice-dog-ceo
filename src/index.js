1464cbe945b8baf0c909b91d286b8b99027a550d
Brad Bieselin <bradley.bieselin@gmail.com>
2022-04-12 17:16:23 -0400

    added fetch images and fetch breeds

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+const fetchDogs = () => {
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(data => addImage(data))
+}
+
+function addImage(pictures) {
+    const imageLink = pictures.message;
+    const img = document.createElement('img');
+
+    imageLink.forEach(element => img.src = element);
+    document.querySelector("#dog-image-container").append(img);
+}
+
+const fetchBreeds = () => {
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(data => addBreed(data))
+}
+
+function addBreed(breeds) {
+    const breedMessage = breeds.message;
+    for(const breed in breedMessage) {
+        const li = document.createElement("li");
+        li.innerText = breed;
+        document.querySelector("#dog-breeds").append(li);
+    }
+}
+
+fetchDogs();
+fetchBreeds();
